6d873c713aeea1a5641bb0fbea5fc18bd8b54b69
daniel vicario <danigv.email@gmail.com>
2018-11-21 18:06:26 +0100

    first commit

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router  = express.Router();
+const multer  = require('multer');
+const Picture = require('../models/picture');
+
+/* GET home page */
+router.get('/', (req, res, next) => {
+  res.render('index');
+});
+
+module.exports = router;
