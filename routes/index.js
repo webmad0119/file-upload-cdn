23f667a9532bfcb0bcc3625298f2ecad68dccea0
Dani Vicario <danigv.email@gmail.com>
2018-11-21 22:43:16 +0100

    added upload

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,30 @@
 const express = require('express');
-const router  = express.Router();
-const multer  = require('multer');
+const router = express.Router();
+const multer = require('multer');
 const Picture = require('../models/picture');
+const upload = multer({ dest: './public/uploads/' });
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get('/', function (req, res, next) {
+  Picture.find((err, pictures) => {
+    res.render('index', {
+      pictures
+    })
+  })
+});
+
+
+
+router.post('/upload', upload.single('photo'), (req, res) => {
+  const pic = new Picture({
+    name: req.body.name,
+    path: `/uploads/${req.file.filename}`,
+    originalName: req.file.originalname
+  });
+
+  pic.save((err) => {
+    res.redirect('/');
+  });
 });
 
 module.exports = router;
